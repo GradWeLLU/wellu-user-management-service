929b0871740783238cd3a45eeec5c8a75fd7b446
Rola Hadidy <rolatotta2004@gmail.com>
2026-04-16 20:02:16 +0200

    remove unused imports

--- src/main/java/com/wellu/usermanagement/service/ProfileService.java
+++ src/main/java/com/wellu/usermanagement/service/ProfileService.java
@@ -6,13 +6,11 @@ import com.wellu.usermanagement.dto.response.UserProfileResponse;
 import com.wellu.usermanagement.entity.User;
 import com.wellu.usermanagement.entity.UserProfile;
 import com.wellu.usermanagement.exception.UserNotFoundException;
-import com.wellu.usermanagement.mapper.UserMapper;
 import com.wellu.usermanagement.mapper.UserProfileMapper;
 import com.wellu.usermanagement.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
